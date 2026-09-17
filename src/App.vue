20d23a4d75ea4686e175c68b7838d66d6381f9ad
caoye41583 <caoye41583@gmail.com>
2026-09-17 15:55:40 +0800

    elementPlus自动按需引入配置

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
     read the documentation
   </p>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
+// 配置elementPlus按需导入
+import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // 配置elementPlus按需导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     // 实际的路径转换 @ -> src
     alias: {
