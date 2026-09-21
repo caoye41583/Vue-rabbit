44c1e95a2a1398ebf82498d9d4fedd462a4a8997
caoye41583 <caoye41583@gmail.com>
2026-09-21 14:55:58 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,6 +6,8 @@
   <HomeNew></HomeNew>
   <HomeHot></HomeHot>
   <HomeProduct></HomeProduct>
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" subtitle="新鲜出炉 品质靠谱"></HomePanel>
 </template>
 
 <script setup lang="ts">
@@ -14,6 +16,7 @@ import HomeNew from "@/views/Home/components/HomeNew.vue";
 import HomeHot from "@/views/Home/components/HomeHot.vue";
 import HomeCategory from "./components/HomeCategory.vue";
 import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePannel.vue";
 </script>
 
 <style scoped></style>
